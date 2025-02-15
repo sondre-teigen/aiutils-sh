a8976a43239bacc91c18944b5111f656f46c42a9
sondre-teigen <sondre.teigen@outlook.com>
2025-02-15 17:32:12 +0100

    Allow renaming files in aiembed

--- src/bin/aiembed.rs
+++ src/bin/aiembed.rs
@@ -5,8 +5,8 @@ use clap::Parser;
 /// Generate embeddings of the contents of the input files
 #[derive(Parser)]
 struct Cli {
-    /// Files to embed. Use - to read stdin
-    files: Vec<PathBuf>,
+    /// Files to embed. Use - to read stdin. Specify <name>:<path> to rename files in the output.
+    files: Vec<String>,
 
     /// File to store embeddings in JSON. Use - to write to stdout
     #[arg(long, default_value = "-")]
@@ -29,13 +29,18 @@ fn main() -> anyhow::Result<()> {
     let mut embeddings = HashMap::new();
 
     for files in args.files.chunks(args.chunk) {
+        let files: Vec<_> = files
+            .iter()
+            .map(|file| file.split_once(":").unwrap_or((&file, &file)))
+            .collect();
+
         let mut inputs = vec![];
-        for file in files {
+        for (_, file) in files.iter() {
             inputs.push(aituils_sh::fs::read_string(file)?);
         }
         let outputs = aituils_sh::api::embed(inputs, &args.model, &key)?;
-        for (file, embedding) in files.iter().zip(outputs) {
-            embeddings.insert(file.to_string_lossy().to_string(), embedding);
+        for ((name, _), embedding) in files.into_iter().zip(outputs) {
+            embeddings.insert(name.to_string(), embedding);
         }
     }
 
